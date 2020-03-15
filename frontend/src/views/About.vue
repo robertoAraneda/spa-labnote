768e507b9ece7492723556cbf5672a543a44e49c
roberto <robaraneda@gmail.com>
2020-03-15 12:00:15 -0300

    second commit

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ quote }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'i want'
+    }
+  }
+}
+</script>
